f662cbfa4a34dd36840af2a82a46cb352713e3d2
Engranpa <l.p.2.c_s@hotmail.com>
2019-05-21 14:48:33 -0300

    CodigoBasicoparte1

---
+++ Evenbrite-app/Assets/Script/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int[] numRandom;
+    public int[] persona;
+    public int i = 0;
+    public int correct = 0;
+    public int casicorrect = 0;
+    void Start()
+    {
+          for (int i = 0; i < 4; i++)
+          {
+              if (numRandom[i] == persona[i])
+              {
+                  correct++;
+              }
+            else if (persona[i] == numRandom[0] || persona[i] == numRandom[1] || persona[i] == numRandom[2] || persona[i] == numRandom[3])
+            {
+                casicorrect++;
+            }
+
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+
+    }
+
+    }
+
